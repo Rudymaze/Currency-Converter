c6b943c32acebb46eeaf428104d46f1b65f75983
RUDYMAZE <njockrudiger4@gmail.com>
2025-03-26 20:59:53 +0200

    fisrt attempt to fetching the currency Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
+const API_KEY = "dc289905fcc47eed5bca6a9e";
+const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+
 function App() {
+  const [currencies, setCurrencies] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState("USD");
   return <></>;
 }
 
