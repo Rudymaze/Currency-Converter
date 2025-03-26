aca3f383a4f485a7ddfd88049ca178e63b53bd79
RUDYMAZE <njockrudiger4@gmail.com>
2025-03-26 21:05:26 +0200

    fetched the Exchange rate Api ans displayed in the console

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
+// API key for fetching and updating the currency converter
 const API_KEY = "dc289905fcc47eed5bca6a9e";
-const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+const MAIN_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
 
 function App() {
   const [currencies, setCurrencies] = useState([]);
   const [fromCurrency, setFromCurrency] = useState("USD");
+
+  // using async and await in case there is any delay and also the try and catch so if there is any error it will be caught
+  useEffect(() => {
+    const fetchExchangeRates = async () => {
+      try {
+        const response = await axios.get(BASE_URL);
+        setExchangeRates(response.data.conversion_rates);
+        setCurrencies(Object.keys(response.data.conversion_rates));
+      } catch (error) {
+        console.error("Error fetching exchange rates:", error);
+      }
+    };
+
+    fetchExchangeRates();
+  }, []);
   return <></>;
 }
 
