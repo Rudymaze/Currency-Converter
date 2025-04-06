36780a01e81176ece1659486cba2224816ee31d1
RUDYMAZE <njockrudiger4@gmail.com>
2025-04-06 10:27:25 +0300

    added a swap currencu button, when clcked will swap currency

--- src/components/CurrencySelector.jsx
+++ src/components/CurrencySelector.jsx
@@ -5,22 +5,33 @@ const CurrencySelector = ({
   selectedCurrency,
   onChange,
   darkMode,
+  label,
 }) => {
   return (
-    <select
-      value={selectedCurrency}
-      onChange={onChange}
-      className={`p-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 w-full ${
-        darkMode
-          ? "bg-gray-700 border-gray-600 text-white focus:ring-blue-500"
-          : "focus:ring-blue-500"
-      }`}>
-      {currencies.map((currency) => (
-        <option key={currency} value={currency}>
-          {currency}
-        </option>
-      ))}
-    </select>
+    <div>
+      {label && (
+        <label
+          className={`block text-sm font-medium mb-1 ${
+            darkMode ? "text-gray-300" : "text-gray-700"
+          }`}>
+          {label}
+        </label>
+      )}
+      <select
+        value={selectedCurrency}
+        onChange={onChange}
+        className={`p-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 w-full ${
+          darkMode
+            ? "bg-gray-700 border-gray-600 text-white focus:ring-blue-500"
+            : "focus:ring-blue-500"
+        }`}>
+        {currencies.map((currency) => (
+          <option key={currency} value={currency}>
+            {currency}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
